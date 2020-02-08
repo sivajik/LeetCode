a6135055d395b2be2ba9667258ebb4e26b2560f0
Sivaji Kondapalli <sivaji_kondapalli@yahoo.com>
2020-02-08 15:53:08 +0530

    Prob1304_FindNUniques

--- src/leetcode/easy/Prob1304_FindNUniques.java
+++ src/leetcode/easy/Prob1304_FindNUniques.java
@@ -17,23 +17,11 @@ public class Prob1304_FindNUniques {
 	public static int[] sumZero(int n) {
 		int[] result = new int[n];
 
-		if (n % 2 == 1) {
-			result[n / 2] = 0;
-			int count = 1;
-			int startIndex = n / 2;
-			for (int i = 1; i <= n / 2; i++) {
-				result[startIndex + i] = count;
-				result[startIndex - i] = -count;
-				count++;
-			}
-		} else {
-			// 0 1 2 3 4 5
-			int count = n / 2;
-			for (int i = 0; i < n / 2; i++) {
-				result[i] = -1 * count;
-				result[i + (n / 2)] = count;
-				count--;
-			}
+		int count = n / 2;
+		for (int i = 0; i < n / 2; i++) {
+			result[i] = -1 * count;
+			result[(n % 2) + i + (n / 2)] = count;
+			count--;
 		}
 
 		return result;
