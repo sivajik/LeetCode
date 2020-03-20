f452140823d94899323a7b5ed3ebb88f9d756ba1
Sivaji Kondapalli <sivaji_kondapalli@yahoo.com>
2020-03-20 11:24:21 +0530

    Prob1365_HowManySmaller

---
+++ src/leetcode/easy/Prob1365_HowManySmaller.java
@@ -0,0 +1,37 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Prob1365_HowManySmaller {
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(smallerNumbersThanCurrent(new int[] { 8, 1, 2, 2, 3 })));
+		System.out.println(Arrays.toString(smallerNumbersThanCurrent(new int[] { 6, 5, 4, 8 })));
+		System.out.println(Arrays.toString(smallerNumbersThanCurrent(new int[] { 7, 7, 7, 7 })));
+	}
+
+	public static int[] smallerNumbersThanCurrent(int[] nums) {
+		int[] orig = new int[nums.length];
+		orig = Arrays.copyOf(nums, nums.length);
+
+		Arrays.sort(nums);
+
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(nums[i])) {
+				map.put(nums[i], map.get(nums[i]));
+			} else {
+				map.put(nums[i], i);
+			}
+		}
+
+		int[] results = new int[nums.length];
+		for (int i = 0; i < orig.length; i++) {
+			results[i] = map.get(orig[i]);
+		}
+
+		return results;
+	}
+}
