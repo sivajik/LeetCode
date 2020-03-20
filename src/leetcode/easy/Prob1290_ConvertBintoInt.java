ac8ba96d7d8c62e788b5d8cd55d8b1a8b7634098
Sivaji Kondapalli <sivaji_kondapalli@yahoo.com>
2020-03-20 11:57:19 +0530

    Prob1290_ConvertBintoInt

---
+++ src/leetcode/easy/Prob1290_ConvertBintoInt.java
@@ -0,0 +1,28 @@
+package leetcode.easy;
+
+public class Prob1290_ConvertBintoInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int getDecimalValue(ListNode head) {
+		StringBuilder sb = new StringBuilder();
+		while (head != null) {
+			sb.append(head.val);
+			head = head.next;
+		}
+		
+		return Integer.parseInt(sb.toString(), 2);
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
