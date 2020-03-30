e2ff98776538eae8c8959fcecfda93fdba435c36
sivajik <sivaji_kondapalli@yahoo.com>
2020-03-30 15:22:59 +0530

    Prob1046_LastStoreWeight

---
+++ src/leetcode/easy/Prob1046_LastStoreWeight.java
@@ -0,0 +1,28 @@
+package leetcode.easy;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Prob1046_LastStoreWeight {
+
+	public static void main(String[] args) {
+		System.out.println(lastStoneWeight(new int[] { 2, 7, 4, 1, 8, 1 }));
+
+	}
+
+	public static int lastStoneWeight(int[] stones) {
+		PriorityQueue<Integer> p = new PriorityQueue<>(Collections.reverseOrder());
+		for (int stone : stones) {
+			p.add(stone);
+		}
+
+		while (p.size() > 1) {
+			int x = p.remove();
+			int y = p.remove();
+			if (x != y) {
+				p.add(x - y);
+			}
+		}
+		return p.size() == 0 ? 0 : p.peek();
+	}
+}
