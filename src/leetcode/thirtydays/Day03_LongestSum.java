4752c24a72019c3db8ef2a879e3bbdbc18933eee
sivajik <sivaji_kondapalli@yahoo.com>
2020-04-03 15:04:32 +0530

    Day03_LongestSum

--- src/leetcode/thirtydays/Day03_LongestSum.java
+++ src/leetcode/thirtydays/Day03_LongestSum.java
@@ -3,20 +3,20 @@ package leetcode.thirtydays;
 public class Day03_LongestSum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(maxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
 
 	}
 
-	public int maxSubArray(int[] a) {
+	public static int maxSubArray(int[] a) {
 		int size = a.length;
-		int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
+		int max_so_far = Integer.MIN_VALUE, tempSum = 0;
 
 		for (int i = 0; i < size; i++) {
-			max_ending_here = max_ending_here + a[i];
-			if (max_so_far < max_ending_here)
-				max_so_far = max_ending_here;
-			if (max_ending_here < 0)
-				max_ending_here = 0;
+			tempSum = tempSum + a[i];
+			if (max_so_far < tempSum)
+				max_so_far = tempSum;
+			if (tempSum < 0)
+				tempSum = 0;
 		}
 		return max_so_far;
 	}
