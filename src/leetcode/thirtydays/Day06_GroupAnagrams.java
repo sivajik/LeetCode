65c729760ce75976e6200d3dfb61576754ff5c0d
sivajik <sivaji_kondapalli@yahoo.com>
2020-04-06 12:43:01 +0530

    Day06_GroupAnagrams

---
+++ src/leetcode/thirtydays/Day06_GroupAnagrams.java
@@ -0,0 +1,37 @@
+package leetcode.thirtydays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Day06_GroupAnagrams {
+
+	public static void main(String[] args) {
+		System.out.println(groupAnagrams(new String[] { "eat", "tea", "tan", "ate", "nat", "bat" }));
+	}
+
+	// brute force appraoch.
+	public static List<List<String>> groupAnagrams(String[] strs) {
+		Map<String, ArrayList<String>> map = new HashMap();
+
+		for (String str : strs) {
+			char[] sortedStr = str.toCharArray();
+			Arrays.sort(sortedStr);
+			String sortedString = new String(sortedStr);
+
+			if (map.containsKey(sortedString)) {
+				ArrayList<String> l = map.get(sortedString);
+				l.add(str);
+				map.put(sortedString, l);
+			} else {
+				ArrayList<String> l = new ArrayList<String>();
+				l.add(str);
+				map.put(sortedString, l);
+			}
+		}
+		java.util.Collection<ArrayList<String>> coll = map.values();
+		return new ArrayList<>(coll);
+	}
+}
