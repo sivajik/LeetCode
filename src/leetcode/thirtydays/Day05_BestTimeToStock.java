a3b937960d2633a4325554b43ccf053d0271a6d5
sivajik <sivaji_kondapalli@yahoo.com>
2020-04-06 12:30:01 +0530

    1,2,3,4,5

---
+++ src/leetcode/thirtydays/Day05_BestTimeToStock.java
@@ -0,0 +1,20 @@
+package leetcode.thirtydays;
+
+public class Day05_BestTimeToStock {
+
+	public static void main(String[] args) {
+		System.out.println(maxProfit(new int[] { 7, 1, 5, 3, 6, 4 }));
+		System.out.println(maxProfit(new int[] { 1, 2, 3, 4, 5 }));
+	}
+
+	public static int maxProfit(int[] prices) {
+		int maxProfit = 0;
+		int j = 0;
+		for (int i = j + 1; i < prices.length; i++) {
+			if (prices[i] > prices[i - 1]) {
+				maxProfit += (prices[i] - prices[i - 1]);
+			}
+		}
+		return maxProfit;
+	}
+}
