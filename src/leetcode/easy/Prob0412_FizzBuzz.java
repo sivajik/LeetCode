cbd203be9c8218ca2cafe61a16f85b1aa0804c8d
sivajik <sivaji_kondapalli@yahoo.com>
2020-04-08 12:56:48 +0530

    Prob0412_FizzBuzz

---
+++ src/leetcode/easy/Prob0412_FizzBuzz.java
@@ -0,0 +1,39 @@
+package leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prob0412_FizzBuzz {
+
+	public static void main(String[] args) {
+		System.out.println(fizzBuzz(15));
+
+	}
+
+	/*
+	 * can't believe this is a leetcode question :(
+	 */
+	public static List<String> fizzBuzz(int n) {
+		List<String> l = new ArrayList<String>();
+		for (int i = 1; i <= n; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				l.add("FizzBuzz");
+				continue;
+			}
+
+			if (i % 3 == 0) {
+				l.add("Fizz");
+				continue;
+			}
+
+			if (i % 5 == 0) {
+				l.add("Buzz");
+				continue;
+			}
+
+			l.add(Integer.toString(i));
+		}
+
+		return l;
+	}
+}
