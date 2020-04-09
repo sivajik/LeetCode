4152d5686501a7865d6de07ed6dbe0dad2fc3787
sivajik <sivaji_kondapalli@yahoo.com>
2020-04-09 12:35:01 +0530

    Day09_BackSpaceStringCompare

--- src/leetcode/thirtydays/Day09_BackSpaceStringCompare.java
+++ src/leetcode/thirtydays/Day09_BackSpaceStringCompare.java
@@ -6,13 +6,17 @@ public class Day09_BackSpaceStringCompare {
 
 	public static void main(String[] args) {
 		System.out.println(backspaceCompare("abcd####", "efgh###"));
-
+		System.out.println(backspaceCompare("ab#c", "ad#c")); // true
+		System.out.println(backspaceCompare("ab##", "c#d#")); // true
+		System.out.println(backspaceCompare("a##c", "#a#c")); // true
+		System.out.println(backspaceCompare("a#c", "b")); // false
 	}
-    public  static boolean backspaceCompare(String S, String T) {
+
+	public static boolean backspaceCompare(String S, String T) {
 		return getString(S).equals(getString(T));
 	}
 
-	public static  String getString(String S) {
+	public static String getString(String S) {
 		Stack<Character> stack = new Stack<Character>();
 
 		for (char c : S.toCharArray()) {
