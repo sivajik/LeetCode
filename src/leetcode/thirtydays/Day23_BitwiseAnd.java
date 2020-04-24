598a80d0e941c89fdeacf9e64db82d23e152965b
sivajik <sivaji_kondapalli@yahoo.com>
2020-04-24 10:03:16 +0530

    Day23_Bitwise

---
+++ src/leetcode/thirtydays/Day23_BitwiseAnd.java
@@ -0,0 +1,16 @@
+package leetcode.thirtydays;
+
+public class Day23_BitwiseAnd {
+
+	public static void main(String[] args) {
+		System.out.println(rangeBitwiseAnd(5, 7));
+		System.out.println(rangeBitwiseAnd(0, 1));
+
+	}
+
+	public static int rangeBitwiseAnd(int m, int n) {
+		while (n > m)
+			n = n & n - 1;
+		return m & n;
+	}
+}
