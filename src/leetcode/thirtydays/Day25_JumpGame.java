9b9fe545f28d42435fa752a47de588f0005c66bf
sivajik <sivaji_kondapalli@yahoo.com>
2020-04-26 09:59:33 +0530

    Day25_JumpGame

---
+++ src/leetcode/thirtydays/Day25_JumpGame.java
@@ -0,0 +1,23 @@
+package leetcode.thirtydays;
+
+public class Day25_JumpGame {
+
+	public static void main(String[] args) {
+		System.out.println(canJump(new int[] { 2, 3, 1, 1, 4 }));
+
+	}
+
+	public static boolean canJump(int[] nums) {
+		if (nums.length <= 1) {
+			return true;
+		}
+		int reach = 0;
+		for (int i = 0; i <= reach; i++) {
+			reach = Math.max(reach, i + nums[i]);
+			if (reach >= nums.length - 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
