d74c9e628a1a365de357fc11b8afba69b8ad379e
sivajik <sivaji_kondapalli@yahoo.com>
2020-04-27 08:42:26 +0530

    Prob1143_LongestCommonSubsequence

---
+++ src/leetcode/medium/Prob1143_LongestCommonSubsequence.java
@@ -0,0 +1,39 @@
+package leetcode.medium;
+
+public class Prob1143_LongestCommonSubsequence {
+
+	public static void main(String[] args) {
+		System.out.println(longestCommonSubsequence("abcdef", "abc"));
+		System.out.println(longestCommonSubsequence("def", "abc"));
+	}
+
+	public static int longestCommonSubsequence(String text1, String text2) {
+		char[] textA = text1.toCharArray();
+		char[] textB = text2.toCharArray();
+
+		int m = textA.length;
+		int n = textB.length;
+
+		int[][] finalArray = new int[m + 1][n + 1];
+
+		for (int i = 0; i < m + 1; i++) {
+			finalArray[i][0] = 0;
+		}
+
+		for (int i = 0; i < n + 1; i++) {
+			finalArray[0][i] = 0;
+		}
+
+		for (int i = 1; i < m + 1; i++) {
+			for (int j = 1; j < n + 1; j++) {
+				if (textA[i - 1] == textB[j - 1]) {
+					finalArray[i][j] = finalArray[i - 1][j - 1] + 1;
+				} else {
+					finalArray[i][j] = Math.max(finalArray[i - 1][j], finalArray[i][j - 1]);
+				}
+			}
+		}
+		return finalArray[m][n];
+	}
+
+}
