223ee4ea9ec459a77be95f785f528bf83c6a4a7e
sivajik <sivaji_kondapalli@yahoo.com>
2020-05-09 14:56:27 +0530

    Day09_PerfectSquare

---
+++ src/leetcode/thirtydays/may/Day09_PerfectSquare.java
@@ -0,0 +1,32 @@
+package leetcode.thirtydays.may;
+
+public class Day09_PerfectSquare {
+
+	public static void main(String[] args) {
+		System.out.println(isPerfectSquare(14));
+	}
+
+	public static boolean isPerfectSquare(int num) {
+		if (num <= 1) {
+			return true;
+		}
+		int lowerLimit = 1;
+		int higherLimit = num;
+		while (lowerLimit <= higherLimit) {
+
+			int midLimit = lowerLimit + (higherLimit - lowerLimit) / 2;
+			if (midLimit * midLimit == num) {
+				return true;
+			}
+
+			int div = num / midLimit;
+
+			if (div >= midLimit) {
+				lowerLimit = midLimit + 1;
+			} else {
+				higherLimit = midLimit - 1;
+			}
+		}
+		return false;
+	}
+}
