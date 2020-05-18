e1854afc0cf6d3a94d674e3d3cf22f9cf05bb0fb
sivajik <sivaji_kondapalli@yahoo.com>
2020-05-18 12:39:56 +0530

    Prob448_NumbersDisappearedInArray

---
+++ src/leetcode/easy/Prob448_NumbersDisappearedInArray.java
@@ -0,0 +1,28 @@
+package leetcode.easy;
+
+import java.util.List;
+
+public class Prob448_NumbersDisappearedInArray {
+
+	public static void main(String[] args) {
+		System.out.println(findDisappearedNumbers(new int[] { 4, 3, 2, 7, 8, 2, 3, 1 }));
+
+	}
+
+	public static List<Integer> findDisappearedNumbers(int[] nums) {
+		List<Integer> l = new java.util.ArrayList<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			int val = Math.abs(nums[i]) - 1;
+			if (nums[val] > 0) {
+				nums[val] = -1 * nums[val];
+			}
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] > 0) {
+				l.add(i + 1);
+			}
+		}
+		return l;
+	}
+}
