78505596a9fa557eb1ec53164403bd2cce1e22e3
sivajik <sivaji_kondapalli@yahoo.com>
2020-05-18 11:08:43 +0530

    242

---
+++ src/leetcode/easy/Prob242_ValidAnagram.java
@@ -0,0 +1,22 @@
+package leetcode.easy;
+
+public class Prob242_ValidAnagram {
+
+	public static void main(String[] args) {
+		System.out.println(isAnagram("anagram", "nagaram"));
+		System.out.println(isAnagram("rat", "cat"));
+	}
+
+	public static boolean isAnagram(String s, String t) {
+		int[] sArray = new int[26];
+		for (char c : s.toCharArray()) {
+			sArray[c - 'a']++;
+		}
+
+		int[] tArray = new int[26];
+		for (char c : t.toCharArray()) {
+			tArray[c - 'a']++;
+		}
+		return java.util.Arrays.equals(sArray, tArray);
+	}
+}
