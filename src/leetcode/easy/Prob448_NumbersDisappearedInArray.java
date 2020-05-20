7de765f5b8a37584ab2f75c2730ef685bf716ca9
sivajik <sivaji_kondapalli@yahoo.com>
2020-05-20 10:44:18 +0530

    Day18_OnlineStockSpan

--- src/leetcode/easy/Prob448_NumbersDisappearedInArray.java
+++ src/leetcode/easy/Prob448_NumbersDisappearedInArray.java
@@ -6,7 +6,6 @@ public class Prob448_NumbersDisappearedInArray {
 
 	public static void main(String[] args) {
 		System.out.println(findDisappearedNumbers(new int[] { 4, 3, 2, 7, 8, 2, 3, 1 }));
-
 	}
 
 	public static List<Integer> findDisappearedNumbers(int[] nums) {
---
+++ src/leetcode/thirtydays/may/Day18_OnlineStockSpan.java
@@ -0,0 +1,44 @@
+package leetcode.thirtydays.may;
+
+public class Day18_OnlineStockSpan {
+
+	public static void main(String[] args) {
+		StockSpanner s = new StockSpanner();
+		System.out.println(s.next(100));
+		System.out.println(s.next(80));
+		System.out.println(s.next(60));
+		System.out.println(s.next(70));
+		System.out.println(s.next(60));
+		System.out.println(s.next(75));
+		System.out.println(s.next(85));
+	}
+
+}
+
+class StockSpanner {
+	class Pair {
+		int price;
+		int span;
+
+		Pair(int price, int span) {
+			this.price = price;
+			this.span = span;
+		}
+	}
+
+	java.util.Stack<Pair> stack = null;
+
+	public StockSpanner() {
+		stack = new java.util.Stack<Pair>();
+	}
+
+	public int next(int price) {
+		int span = 1;
+		while (!stack.isEmpty() && stack.peek().price <= price) {
+			int topSpan = stack.pop().span;
+			span += topSpan;
+		}
+		stack.push(new Pair(price, span));
+		return span;
+	}
+}
