812cd879e24e627aafda530484f7fcb8b9b52345
sivajik <sivaji_kondapalli@yahoo.com>
2020-05-29 18:16:27 +0530

    Prob100_SameTree

---
+++ src/leetcode/easy/Prob100_SameTree.java
@@ -0,0 +1,70 @@
+package leetcode.easy;
+
+public class Prob100_SameTree {
+
+	public static void main(String[] args) {
+		TreeNode p = prepareTree(new int[] { 1, 2, 3 });
+		TreeNode q = prepareTree(new int[] { 1, 2, 3 });
+		System.out.println(isSameTree(p, q));
+	}
+
+	public static boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		}
+		if ((p == null && q != null) || (p != null && q == null)) {
+			return false;
+		}
+		if (p.val != q.val) {
+			return false;
+		}
+		return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	}
+
+	public static TreeNode prepareTree(int[] values) {
+		TreeNode[] treeArray = new TreeNode[values.length];
+		for (int i = 0; i < values.length; i++) {
+			if (values[i] == -999) {
+				treeArray[i] = null;
+			} else {
+				treeArray[i] = new TreeNode(values[i]);
+			}
+		}
+
+		for (int i = 0; i < values.length; i++) {
+			TreeNode node = treeArray[i];
+			if (((2 * i) + 1 < values.length) && ((2 * i) + 2 < values.length)) {
+				node.left = treeArray[(2 * i) + 1];
+				node.right = treeArray[(2 * i) + 2];
+			}
+		}
+		return treeArray[0];
+	}
+
+	public static void inOrder(TreeNode tree) {
+		if (tree != null) {
+			inOrder(tree.left);
+			System.out.print(tree.val + " ");
+			inOrder(tree.right);
+		}
+	}
+
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
