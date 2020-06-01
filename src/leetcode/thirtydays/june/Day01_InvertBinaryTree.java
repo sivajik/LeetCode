cdc891a4b08331916f67181d732de2f7b30d3236
sivajik <sivaji_kondapalli@yahoo.com>
2020-06-01 13:37:19 +0530

    Day01_InvertBinaryTree

---
+++ src/leetcode/thirtydays/june/Day01_InvertBinaryTree.java
@@ -0,0 +1,70 @@
+package leetcode.thirtydays.june;
+
+public class Day01_InvertBinaryTree {
+
+	public static void main(String[] args) {
+		TreeNode root = prepareTree(new int[] { 4, 2, 7, 1, 3, 6, 9 });
+		inOrder(root);
+		System.out.println();
+		TreeNode nn = invertTree(root);
+		inOrder(nn);
+	}
+
+	static public TreeNode invertTree(TreeNode root) {
+		if (root != null) {
+			invertTree(root.left);
+			invertTree(root.right);
+			TreeNode temp = root.left;
+			root.left = root.right;
+			root.right = temp;
+		}
+		return root;
+	}
+
+	public static TreeNode prepareTree(int[] values) {
+		TreeNode[] treeArray = new TreeNode[values.length];
+		for (int i = 0; i < values.length; i++) {
+			if (values[i] == -999) {
+				treeArray[i] = null;
+			} else {
+				treeArray[i] = new TreeNode(values[i]);
+			}
+		}
+
+		for (int i = 0; i < values.length; i++) {
+			TreeNode node = treeArray[i];
+			if (((2 * i) + 1 < values.length) && ((2 * i) + 2 < values.length)) {
+				node.left = treeArray[(2 * i) + 1];
+				node.right = treeArray[(2 * i) + 2];
+			}
+		}
+		return treeArray[0];
+	}
+
+	public static void inOrder(TreeNode tree) {
+		if (tree != null) {
+			inOrder(tree.left);
+			System.out.print(tree.val + " ");
+			inOrder(tree.right);
+		}
+	}
+
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
