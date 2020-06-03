38f38a9d0d9a3c2b4afa6fa18443ae588ddd18bd
sivajik <sivaji_kondapalli@yahoo.com>
2020-06-03 18:11:51 +0530

    Prob0066_PlusOne

--- src/leetcode/easy/Prob0066_PlusOne.java
+++ src/leetcode/easy/Prob0066_PlusOne.java
@@ -5,32 +5,27 @@ import java.util.Arrays;
 public class Prob0066_PlusOne {
 
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(plusOne(new int[] { 0 })));
-		System.out.println(Arrays.toString(plusOne(new int[] { 4 })));
-		System.out.println(Arrays.toString(plusOne(new int[] { 9 })));
-		System.out.println(Arrays.toString(plusOne(new int[] { 1, 2, 3 })));
-		System.out.println(Arrays.toString(plusOne(new int[] { 1, 2, 9 })));
-		System.out.println(Arrays.toString(plusOne(new int[] { 4, 3, 2, 1 })));
-		System.out.println(Arrays.toString(plusOne(new int[] { 9, 8, 9 })));
-		System.out.println(Arrays.toString(plusOne(new int[] { 9, 9, 9 })));
+		System.out.println(Arrays.toString(plusOne(new int[] { 0 }))); // 1
+		System.out.println(Arrays.toString(plusOne(new int[] { 4 }))); // 5
+		System.out.println(Arrays.toString(plusOne(new int[] { 9 }))); // 10
+		System.out.println(Arrays.toString(plusOne(new int[] { 1, 2, 3 }))); // 124
+		System.out.println(Arrays.toString(plusOne(new int[] { 1, 2, 9 }))); // 130
+		System.out.println(Arrays.toString(plusOne(new int[] { 4, 3, 2, 1 }))); // 4322
+		System.out.println(Arrays.toString(plusOne(new int[] { 9, 8, 9 }))); // 990
+		System.out.println(Arrays.toString(plusOne(new int[] { 9, 9, 9 }))); // 1000
 	}
 
 	public static int[] plusOne(int[] digits) {
-		int carryOver = 0, addendum = 0;
 		for (int i = digits.length - 1; i >= 0; i--) {
-			int digit = digits[i];
-			addendum = (i == digits.length - 1) ? 1 : 0;
-			digits[i] = (digit + addendum + carryOver) % 10;
-			carryOver = (digit + addendum + carryOver) / 10;
+			if (digits[i] < 9) {
+				digits[i] = digits[i] + 1;
+				return digits;
+			} else {
+				digits[i] = 0;
+			}
 		}
-
-		if (carryOver != 0) {
-			int[] newDigits = new int[digits.length + 1];
-			newDigits[0] = carryOver;
-			System.arraycopy(digits, 0, newDigits, 1, digits.length);
-			return newDigits;
-		} else {
-			return digits;
-		}
+		int[] newNumber = new int[digits.length + 1];
+		newNumber[0] = 1;
+		return newNumber;
 	}
 }
