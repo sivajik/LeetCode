cca269a7f1aa0f2f00c4c86157bd6c7010ca914f
sivajik <sivaji_kondapalli@yahoo.com>
2020-06-09 15:38:23 +0530

    Day 09

---
+++ src/leetcode/thirtydays/june/Day09_IsSubSequence.java
@@ -0,0 +1,40 @@
+package leetcode.thirtydays.june;
+
+public class Day09_IsSubSequence {
+
+
+	public static void main(String[] args) {
+		System.out.println(isSubsequence("abcd", "xabcdefg"));
+	}
+
+	static public boolean isSubsequence(String s, String t) {
+
+		if (s == null || s.length() == 0) {
+			return true;
+		}
+		if (s.length() > t.length()) {
+			return false;
+		}
+
+		int sPtr = 0;
+		int tPtr = 0;
+
+		int sLen = s.length();
+		int tLen = t.length();
+
+		char[] sArr = s.toCharArray();
+		char[] tArr = t.toCharArray();
+
+		while (sPtr < sLen && tPtr < tLen) {
+			if (sArr[sPtr] != tArr[tPtr]) {
+				tPtr++;
+			} else {
+				sPtr++;
+				tPtr++;
+			}
+		}
+		return sPtr == sLen && tPtr <= tLen;
+
+	}
+
+}
