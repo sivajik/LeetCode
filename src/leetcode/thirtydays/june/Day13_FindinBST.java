bdf4cba2ec7c5711f67f004453d9f63329a66ec4
sivajik <sivaji_kondapalli@yahoo.com>
2020-06-16 05:41:36 +0530

    Day13_FindinBST

---
+++ src/leetcode/thirtydays/june/Day13_FindinBST.java
@@ -0,0 +1,70 @@
+package leetcode.thirtydays.june;
+
+public class Day13_FindinBST {
+
+	public static void main(String[] args) {
+		System.out.println(searchBST(prepareTree(new int[] { 4, 2, 7, 1, 3 }), 1).val);
+	}
+
+	static public TreeNode searchBST(TreeNode root, int val) {
+
+		if (root == null) {
+			return null;
+		}
+		if (root.val == val) {
+			return root;
+		} else if (val < root.val) {
+			return searchBST(root.left, val);
+		} else {
+			return searchBST(root.right, val);
+		}
+
+	}
+
+	public static TreeNode prepareTree(int[] values) {
+		TreeNode[] treeArray = new TreeNode[values.length];
+		for (int i = 0; i < values.length; i++) {
+			if (values[i] == -999) {
+				treeArray[i] = null;
+			} else {
+				treeArray[i] = new TreeNode(values[i]);
+			}
+		}
+
+		for (int i = 0; i < values.length; i++) {
+			TreeNode node = treeArray[i];
+			if (((2 * i) + 1 < values.length) && ((2 * i) + 2 < values.length)) {
+				node.left = treeArray[(2 * i) + 1];
+				node.right = treeArray[(2 * i) + 2];
+			}
+		}
+		return treeArray[0];
+	}
+
+	public static void inOrder(TreeNode tree) {
+		if (tree != null) {
+			inOrder(tree.left);
+			System.out.print(tree.val + " ");
+			inOrder(tree.right);
+		}
+	}
+
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
