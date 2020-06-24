121bc637ded7ce874d692d515a72f6ebd89aee82
sivajik <sivaji_kondapalli@yahoo.com>
2020-06-25 05:17:07 +0530

    Prob198_HouseRobber - So Happy

---
+++ src/leetcode/easy/Prob198_HouseRobber.java
@@ -0,0 +1,28 @@
+package leetcode.easy;
+
+public class Prob198_HouseRobber {
+
+	public static void main(String[] args) {
+		System.out.println(rob(new int[] { 1, 2, 3, 1 }));
+		System.out.println(rob(new int[] { 2, 7, 9, 3, 1 }));
+		System.out.println(rob(new int[] {}));
+		System.out.println(rob(new int[] {0}));
+	}
+
+	static public int rob(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
+		if (nums.length == 1) {
+			return nums[0];
+		}
+		int[] dp = new int[nums.length + 1];
+
+		dp[0] = nums[0];
+		dp[1] = Math.max(nums[0], nums[1]);
+		for (int i = 2; i < nums.length; i++) {
+			dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+		}
+		return dp[nums.length - 1];
+	}
+}
