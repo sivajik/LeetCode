f36ccc274c70cb2f043c7d5237696685d7124c6b
sivajik <sivaji_kondapalli@yahoo.com>
2020-06-30 09:30:01 +0530

    Day29_UniquePaths

---
+++ src/leetcode/thirtydays/june/Day29_UniquePaths.java
@@ -0,0 +1,28 @@
+package leetcode.thirtydays.june;
+
+public class Day29_UniquePaths {
+	public static void main(String[] args) {
+		System.out.println(uniquePaths(7, 3));
+		System.out.println(uniquePaths(3, 2));
+		System.out.println(uniquePaths(1, 1));
+		System.out.println(uniquePaths(100, 100));
+	}
+
+	static public int uniquePaths(int m, int n) {
+		int rows = n;
+		int cols = m;
+		int[][] dp = new int[rows][cols];
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				if (i == 0 || j == 0) {
+					dp[i][j] = 1;
+				} else {
+					dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+				}
+			}
+		}
+		return dp[rows - 1][cols - 1];
+	}
+
+}
