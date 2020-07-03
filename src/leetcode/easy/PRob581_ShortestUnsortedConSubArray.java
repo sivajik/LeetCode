48dc157e71391f04c07a9206dd67ec6c8b7c237f
sivajik <sivaji_kondapalli@yahoo.com>
2020-07-03 10:37:34 +0530

    PRob581_ShortestUnsortedConSubArray

---
+++ src/leetcode/easy/PRob581_ShortestUnsortedConSubArray.java
@@ -0,0 +1,36 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+
+public class PRob581_ShortestUnsortedConSubArray {
+
+	public static void main(String[] args) {
+		System.out.println(findUnsortedSubarray(new int[] { 2, 6, 4, 8, 10, 9, 15 }));
+		System.out.println(findUnsortedSubarray(new int[] { 2, 3, 4 }));
+		System.out.println(findUnsortedSubarray(new int[] { 2, 5, 3, 8 }));
+		System.out.println(findUnsortedSubarray(new int[] { 2 }));
+		System.out.println(findUnsortedSubarray(new int[] { 2, 1 }));
+	}
+
+	static public int findUnsortedSubarray(int[] nums) {
+		int[] sorted = Arrays.copyOf(nums, nums.length);
+		Arrays.sort(sorted);
+
+		int low = -1, high = -1;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != sorted[i]) {
+				if (low == -1) {
+					low = i;
+				} else {
+					high = i;
+				}
+			}
+		}
+		return high - low == 0 ? 0 : high - low + 1;
+	}
+}
+/*
+ * 
+ * 2, 6, 4, 8, 10, 9, 15 2, 4, 6, 8, 9,10, 15
+ * 
+ */
