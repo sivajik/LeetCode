d3953a6e95eca029f15ddf4f4e9c20f82a58d369
sivajik <sivaji_kondapalli@yahoo.com>
2020-07-07 17:13:33 +0530

    Prob1476_SubrectangleQueries

---
+++ src/leetcode/medium/Prob1476_SubrectangleQueries.java
@@ -0,0 +1,42 @@
+package leetcode.medium;
+
+public class Prob1476_SubrectangleQueries {
+
+	public static void main(String[] args) {
+		SubrectangleQueries s = new SubrectangleQueries(new int[][] { { 1, 1, 1 }, { 2, 2, 2 }, { 3, 3, 3 } });
+		System.out.println(s.getValue(0, 0) == 1);
+		s.updateSubrectangle(0, 0, 2, 2, 100);
+		System.out.println(s.getValue(2, 2) == 100);
+		s.updateSubrectangle(1, 1, 2, 2, 20);
+		System.out.println(s.getValue(2, 2) == 20);
+	}
+}
+
+class SubrectangleQueries {
+	int[][] data;
+
+	public SubrectangleQueries(int[][] rectangle) {
+		/*int rows = rectangle.length;
+		int cols = rectangle[0].length;
+
+		this.data = new int[rows][cols];
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				this.data[i][j] = rectangle[i][j];
+			}
+		}*/
+		this.data = rectangle;
+	}
+
+	public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+		for (int i = row1; i <= row2; i++) {
+			for (int j = col1; j <= col2; j++) {
+				this.data[i][j] = newValue;
+			}
+		}
+	}
+
+	public int getValue(int row, int col) {
+		return this.data[row][col];
+	}
+}
