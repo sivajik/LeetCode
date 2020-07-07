d8ecc5e6bacb0b3aeec2d6b889d4eb744f0c801c
sivajik <sivaji_kondapalli@yahoo.com>
2020-07-07 06:23:49 +0530

    Day06_PlusOne

---
+++ src/leetcode/thirtydays/july/Day06_PlusOne.java
@@ -0,0 +1,32 @@
+package leetcode.thirtydays.july;
+
+import java.util.Arrays;
+
+public class Day06_PlusOne {
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(plusOne(new int[] { 0 }))); // 1
+		System.out.println(Arrays.toString(plusOne(new int[] { 4 }))); // 5
+		System.out.println(Arrays.toString(plusOne(new int[] { 9 }))); // 10
+		System.out.println(Arrays.toString(plusOne(new int[] { 1, 2, 3 }))); // 124
+		System.out.println(Arrays.toString(plusOne(new int[] { 1, 2, 9 }))); // 130
+		System.out.println(Arrays.toString(plusOne(new int[] { 4, 3, 2, 1 }))); // 4322
+		System.out.println(Arrays.toString(plusOne(new int[] { 9, 8, 9 }))); // 990
+		System.out.println(Arrays.toString(plusOne(new int[] { 9, 9, 9 }))); // 1000
+	}
+
+	public static int[] plusOne(int[] digits) {
+		for (int i = digits.length - 1; i >= 0; i--) {
+			if (digits[i] < 9) {
+				digits[i] = digits[i] + 1;
+				return digits;
+			} else {
+				digits[i] = 0;
+			}
+		}
+		int[] newNumber = new int[digits.length + 1];
+		newNumber[0] = 1;
+		return newNumber;
+	}
+
+}
