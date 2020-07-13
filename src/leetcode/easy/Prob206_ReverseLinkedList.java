192114d5fc0af5bd48056202cd71a847879e2560
sivajik <sivaji_kondapalli@yahoo.com>
2020-07-13 11:17:03 +0530

    206

--- src/leetcode/easy/Prob206_ReverseLinkedList.java
+++ src/leetcode/easy/Prob206_ReverseLinkedList.java
@@ -2,7 +2,30 @@ package leetcode.easy;
 
 public class Prob206_ReverseLinkedList {
 	public static void main(String[] args) throws Exception {
+		ListNode n1 = new ListNode(1);
+		ListNode n2 = new ListNode(2);
+		ListNode n3 = new ListNode(3);
+		ListNode n4 = new ListNode(4);
+		ListNode n5 = new ListNode(5);
+		ListNode n6 = new ListNode(6);
 
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		n5.next = n6;
+
+		print(n1);
+		print(reverseList(n1));
+	}
+
+	private static void print(ListNode n1) {
+		ListNode temp = n1;
+		while (temp != null) {
+			System.out.print(temp.val + " ");
+			temp = temp.next;
+		}
+		System.out.println();
 	}
 
 	public static ListNode reverseList(ListNode head) {
@@ -18,7 +41,7 @@ public class Prob206_ReverseLinkedList {
 		return prev;
 	}
 
-	class ListNode {
+	static class ListNode {
 		int val;
 		ListNode next;
 
