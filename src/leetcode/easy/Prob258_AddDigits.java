386146422872ec80fc261a0d1fd04cbe4177c486
sivajik <sivaji_kondapalli@yahoo.com>
2020-07-27 10:27:07 +0530

    Prob258_AddDigits

--- src/leetcode/easy/Prob258_AddDigits.java
+++ src/leetcode/easy/Prob258_AddDigits.java
@@ -7,12 +7,20 @@ public class Prob258_AddDigits {
 		System.out.println(addDigits(1371981));
 		System.out.println(addDigits(29101986));
 		System.out.println(addDigits(6101956));
-		
+
 		System.out.println(addDigits(13071982));
 
 	}
 
 	public static int addDigits(int num) {
+		if (num == 0)
+			return 0;
+		if (num % 9 == 0)
+			return 9;
+		return num % 9;
+	}
+
+	public static int addDigits1(int num) {
 		if (num < 10) {
 			return num;
 		}
