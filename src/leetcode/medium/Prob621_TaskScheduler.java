50620d47d51839cd0727c8949c66312a6e693905
sivajik <sivaji_kondapalli@yahoo.com>
2020-07-29 12:04:02 +0530

    Prob621_TaskScheduler

---
+++ src/leetcode/medium/Prob621_TaskScheduler.java
@@ -0,0 +1,25 @@
+package leetcode.medium;
+
+public class Prob621_TaskScheduler {
+
+	public static void main(String[] args) {
+		System.out.println(leastInterval(new char[] { 'A', 'A', 'A', 'B', 'B', 'B' }, 2));
+
+	}
+
+	static public int leastInterval(char[] tasks, int n) {
+		int[] charMap = new int[26];
+		for (char c : tasks) {
+			charMap[c - 'A']++;
+		}
+		java.util.Arrays.sort(charMap);
+		int maxVal = charMap[25] - 1;
+		int idleslots = maxVal * n;
+
+		for (int i = 24; i >= 0; i--) {
+			idleslots -= Math.min(charMap[i], maxVal);
+		}
+
+		return idleslots > 0 ? idleslots + tasks.length : tasks.length;
+	}
+}
