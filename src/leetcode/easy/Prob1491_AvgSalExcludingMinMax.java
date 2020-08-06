9eb1ae0bfaeaf844dd8dbe88743c2dd25dbc7cbd
sivajik <sivaji_kondapalli@yahoo.com>
2020-08-06 12:28:35 +0530

    Prob1491_AvgSalExcludingMinMax

--- src/leetcode/easy/Prob1491_AvgSalExcludingMinMax.java
+++ src/leetcode/easy/Prob1491_AvgSalExcludingMinMax.java
@@ -8,8 +8,8 @@ public class Prob1491_AvgSalExcludingMinMax {
 		System.out.println(average(new int[] {6000,5000,4000,3000,2000,1000}));
 		System.out.println(average(new int[] {8000,9000,2000,3000,6000,1000}));
 	}
-
-	static public double average(int[] salary) {
+	// 28%
+	static public double average1(int[] salary) {
 		double avg = 0;
 		java.util.Arrays.sort(salary);
 
@@ -17,5 +17,22 @@ public class Prob1491_AvgSalExcludingMinMax {
 			avg += salary[i];
 		}
 		return avg / (salary.length - 2);
+	}
+	// 100%
+	static public double average(int[] salary) {
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		
+		double avg = 0;
+		for (int sal : salary) {
+			if (sal < min) {
+				min = sal;
+			}
+			if (sal > max) {
+				max = sal;
+			}
+			avg += sal;
+		}
+		return (avg-min-max) / (salary.length - 2);
 	}
 }
