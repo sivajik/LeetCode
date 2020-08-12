554e373257b0e56b99d6cb244240fc372938fc31
sivajik <sivaji_kondapalli@yahoo.com>
2020-08-12 10:22:01 +0530

    Prob274_HIndex

---
+++ src/leetcode/medium/Prob274_HIndex.java
@@ -0,0 +1,24 @@
+package leetcode.medium;
+
+import java.util.Arrays;
+
+public class Prob274_HIndex {
+
+	public static void main(String[] args) {
+		System.out.println(hIndex(new int[] { 3, 0, 6, 1, 5 }));
+	}
+
+	static public int hIndex(int[] citations) {
+		Arrays.sort(citations);
+		int l = 0, h = citations.length - 1;
+		while (l <= h) { // 0 1 3 5 6
+			int mid = l + (h - l) / 2;
+			if (citations[mid] < citations.length - mid) {
+				l = mid + 1;
+			} else {
+				h = mid - 1;
+			}
+		}
+		return citations.length - l;
+	}
+}
