63604fc076b160abe20b5190b3343751e8afc6cd
sivajik <sivaji_kondapalli@yahoo.com>
2020-08-22 10:32:08 +0530

    D21

---
+++ src/leetcode/thirtydays/august/Day21_SortArrayByParity.java
@@ -0,0 +1,26 @@
+package leetcode.thirtydays.august;
+
+import java.util.Arrays;
+
+public class Day21_SortArrayByParity {
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(sortArrayByParity(new int[] { 3, 1, 2, 4 })));
+
+	}
+
+	public static int[] sortArrayByParity(int[] A) {
+		int[] op = new int[A.length];
+		int i = 0;
+		int j = A.length - 1;
+
+		for (int num : A) {
+			if (num % 2 == 0) {
+				op[i++] = num;
+			} else {
+				op[j--] = num;
+			}
+		}
+		return op;
+	}
+}
