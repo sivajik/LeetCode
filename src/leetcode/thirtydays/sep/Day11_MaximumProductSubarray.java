2f639141fd92d5bb871c6e5ac8f53f6a9d269191
sivajik <sivaji_kondapalli@yahoo.com>
2020-09-12 07:00:42 +0530

    11

---
+++ src/leetcode/thirtydays/sep/Day11_MaximumProductSubarray.java
@@ -0,0 +1,31 @@
+package leetcode.thirtydays.sep;
+
+public class Day11_MaximumProductSubarray {
+
+	public static void main(String[] args) {
+		System.out.println(maxProduct(new int[] { 2, 3, -2, 4 }));
+		System.out.println(maxProduct(new int[] { -2, 0, -1 }));
+	}
+
+	static public int maxProduct(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return -1;
+		}
+
+		int localMax = nums[0];
+		int localMin = nums[0];
+		int globalMax = nums[0];
+
+		for (int i = 1; i < nums.length; i++) {
+			int temp = localMax;
+
+			localMax = Math.max(nums[i], Math.max(localMax * nums[i], localMin * nums[i]));
+			localMin = Math.max(nums[i], Math.max(temp * nums[i], localMin * nums[i]));
+
+			if (localMax > globalMax) {
+				globalMax = localMax;
+			}
+		}
+		return globalMax;
+	}
+}
