44d09b7283e4bede6b8726fd4b617a0759a75e9d
sivajik <sivaji_kondapalli@yahoo.com>
2020-10-27 20:22:40 +0000

    D27

---
+++ src/leetcode/thirtydays/oct/Day27_LinkedListCycle2.java
@@ -0,0 +1,58 @@
+package leetcode.thirtydays.oct;
+
+public class Day27_LinkedListCycle2 {
+
+	public static void main(String[] args) {
+		ListNode n0 = new ListNode(3);
+		ListNode n1 = new ListNode(2);
+		ListNode n2 = new ListNode(0);
+		ListNode n3 = new ListNode(4);
+
+		n0.next = n1;
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n1;
+
+		System.out.println(detectCycle(n0).val);
+	}
+
+	static public ListNode detectCycle(ListNode head) {
+		if (head == null || head.next == null) {
+			return null;
+		}
+
+		ListNode slow = head;
+		ListNode fast = head;
+		boolean cycleFound = false;
+		while (slow != null && fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+
+			if (slow == fast) {
+				cycleFound = true;
+				break;
+			}
+		}
+
+		if (!cycleFound) {
+			return null;
+		} else {
+			slow = head;
+			while (slow != fast) {
+				slow = slow.next;
+				fast = fast.next;
+			}
+		}
+		return slow;
+	}
+
+	static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+}
