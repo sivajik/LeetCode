2324cfb638fdb7863b9f0b279027eaa325ee4e10
sivajik <sivaji_kondapalli@yahoo.com>
2020-11-03 20:44:00 +0000

    D3

---
+++ src/leetcode/thirtydays/nov/Day03_ConsecutiveChars.java
@@ -0,0 +1,36 @@
+package leetcode.thirtydays.nov;
+
+public class Day03_ConsecutiveChars {
+
+	public static void main(String[] args) {
+		System.out.println(maxPower("leetcode"));
+		System.out.println(maxPower("abbcccddddeeeeedcba"));
+		System.out.println(maxPower("triplepillooooow"));
+		System.out.println(maxPower("hooraaaaaaaaaaay"));
+		System.out.println(maxPower("tourist"));
+
+	}
+
+	public static int maxPower(String s) {
+		if (s.length() == 1) {
+			return 1;
+		}
+
+		int max = Integer.MIN_VALUE;
+		char[] c = s.toCharArray();
+		char prev = c[0];
+		int cCount = 1;
+		for (int i = 1; i < c.length; i++) {
+			if (c[i] == prev) {
+				cCount++;
+				if (cCount > max) {
+					max = cCount;
+				}
+			} else {
+				cCount = 1;
+				prev = c[i];
+			}
+		}
+		return max == Integer.MIN_VALUE ? 1 : max;
+	}
+}
