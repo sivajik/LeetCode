09c48fac9a5b9b40e7df10e67d11fd4974aadbec
sivajik <sivaji_kondapalli@yahoo.com>
2020-11-19 20:54:13 +0000

    P1379_FindInClone

---
+++ src/leetcode/revision/week01/medium/P1379_FindInClone.java
@@ -0,0 +1,42 @@
+package leetcode.revision.week01.medium;
+
+public class P1379_FindInClone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+		if (original == null) {
+			return null;
+		}
+		if (original == target) {
+			return cloned;
+		}
+		TreeNode left = getTargetCopy(original.left, cloned.left, target);
+		if (left != null) {
+			return left;
+		}
+		return getTargetCopy(original.right, cloned.right, target);
+	}
+
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
