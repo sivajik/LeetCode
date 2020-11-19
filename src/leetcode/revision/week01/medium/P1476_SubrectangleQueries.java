80e6d3e75b77214a56a3b5fd4272a10cec7a1344
sivajik <sivaji_kondapalli@yahoo.com>
2020-11-19 20:19:18 +0000

    P1476_SubrectangleQueries

---
+++ src/leetcode/revision/week01/medium/P1476_SubrectangleQueries.java
@@ -0,0 +1,34 @@
+package leetcode.revision.week01.medium;
+
+public class P1476_SubrectangleQueries {
+
+	public static void main(String[] args) {
+		SubrectangleQueries s = new SubrectangleQueries(new int[][] { { 1, 1, 1 }, { 2, 2, 2 }, { 3, 3, 3 } });
+		System.out.println(s.getValue(0, 0) == 1);
+		s.updateSubrectangle(0, 0, 2, 2, 100);
+		System.out.println(s.getValue(2, 2) == 100);
+		s.updateSubrectangle(1, 1, 2, 2, 20);
+		System.out.println(s.getValue(2, 2) == 20);
+	}
+
+}
+
+class SubrectangleQueries {
+	int[][] rect;
+
+	public SubrectangleQueries(int[][] rectangle) {
+		this.rect = rectangle;
+	}
+
+	public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+		for (int i = row1; i <= row2; i++) {
+			for (int j = col1; j <= col2; j++) {
+				this.rect[i][j] = newValue;
+			}
+		}
+	}
+
+	public int getValue(int row, int col) {
+		return this.rect[row][col];
+	}
+}
