a8bf60bd1eac35ded2b4f60fabeb623b7794e16a
sivajik <sivaji_kondapalli@yahoo.com>
2020-11-25 21:31:00 +0000

    peak

--- src/leetcode/revision/week02/P162_FindPeakElement.java
+++ src/leetcode/revision/week02/P162_FindPeakElement.java
@@ -4,18 +4,19 @@ public class P162_FindPeakElement {
 
 	public static void main(String[] args) {
 		// System.out.println(findPeakElement1(new int[] { 1, 2, 1, 3, 5, 6, 4 }));
-		System.out.println(findPeakElement1(new int[] { 3,4,3,2,1}));
+		System.out.println(findPeakElement1(new int[] { 3, 4, 3, 2, 1 }));
+		System.out.println(findPeakElement1(new int[] { 5, 1, 2, 3, 4 }));
 	}
 
 	static public int findPeakElement1(int[] nums) {
 		if (nums == null || nums.length == 0) {
 			return -1;
 		}
-		
+
 		if (nums.length == 1) {
 			return 0;
 		}
-		
+
 		if (nums.length == 2) {
 			if (nums[0] > nums[1]) {
 				return 0;
@@ -28,22 +29,21 @@ public class P162_FindPeakElement {
 		int h = nums.length - 1;
 		while (l <= h) {
 			if (l + 1 == h) {
-				return nums[l] > nums[h]? l : h;
+				return nums[l] > nums[h] ? l : h;
 			}
-			
-			
+
 			int mid = l + (h - l) / 2;
 
 			if (mid > 0 && mid < nums.length - 1) {
-				if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
+				if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
 					return mid;// THE mid element is bigger than both sides.
-				} else if (nums[mid] > nums[mid-1]) {
+				} else if (nums[mid] > nums[mid - 1]) {
 					// only mid is bigger than left, smaller than right so move forward
 					l = mid + 1;
 				} else {
-					h = mid -1;
+					h = mid - 1;
 				}
-				
+
 			} else {
 				if (mid == 0) {
 					if (nums[mid] > nums[mid + 1]) {
