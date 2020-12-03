352a409a39152077dba9cffac6c661df630a52f9
sivajik <sivaji_kondapalli@yahoo.com>
2020-12-03 20:07:46 +0000

    1265

---
+++ src/leetcode/medium/Prob1265_PrintImmutableLinkedListInReverse.java
@@ -0,0 +1,22 @@
+package leetcode.medium;
+
+public class Prob1265_PrintImmutableLinkedListInReverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    static public void printLinkedListInReverse(ImmutableListNode head) {
+    	// dont use stack and pop in sequence. rather use recursion
+        if (head == null) {
+        	return;
+        }
+        printLinkedListInReverse(head.getNext());
+        head.printValue();
+    }
+}
+
+interface ImmutableListNode {
+	public void printValue();
+	public ImmutableListNode getNext();
+}
