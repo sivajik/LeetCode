1e1d16f83c814f4a64968c61170aa19f22eaaab8
sivajik <sivaji_kondapalli@yahoo.com>
2020-12-10 20:38:07 +0000

    Prob941_ValidMountainArray

---
+++ src/leetcode/easy/Prob941_ValidMountainArray.java
@@ -0,0 +1,30 @@
+package leetcode.easy;
+
+public class Prob941_ValidMountainArray {
+
+	public static void main(String[] args) {
+		System.out.println(validMountainArray(new int[] { 2, 1 }));
+		System.out.println(validMountainArray(new int[] { 3, 5, 5 }));
+		System.out.println(validMountainArray(new int[] { 0, 3, 2, 1 }));
+		System.out.println(validMountainArray(new int[] { 1,3,2 }));
+	}
+
+	static public boolean validMountainArray(int[] arr) {
+		if (arr == null || arr.length == 0 || arr.length < 3) {
+			return false;
+		}
+
+		int low = 0;
+		int high = arr.length - 1;
+
+		while (low < arr.length - 1 && arr[low] < arr[low + 1]) {
+			low = low + 1;
+		}
+
+		while (high > 0 && arr[high] < arr[high - 1]) {
+			high = high - 1;
+		}
+
+		return low == high && low != 0 && high != arr.length - 1;
+	}
+}
