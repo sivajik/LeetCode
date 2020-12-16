80f4a78ea7d78fbeb65c052a0a3ea87bf81b467f
sivajik <sivaji_kondapalli@yahoo.com>
2020-12-16 19:03:21 +0000

    D16

---
+++ src/leetcode/thirtydays/dec/Day16_ValidateBST.java
@@ -0,0 +1,42 @@
+package leetcode.thirtydays.dec;
+
+public class Day16_ValidateBST {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean isValidBST(TreeNode root) {
+		return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+	}
+
+	private boolean helper(TreeNode root, long minValue, long maxValue) {
+		if (root == null) {
+			return true;
+		}
+		if (root.val <= minValue || root.val >= maxValue) {
+			return false;
+		}
+		return helper(root.left, minValue, root.val) && helper(root.right, root.val, maxValue);
+	}
+
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
