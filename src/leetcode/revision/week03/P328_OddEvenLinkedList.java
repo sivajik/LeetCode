ab53581cafd29d7c35076c4b6ce534d7b82031a3
sivajik <sivaji_kondapalli@yahoo.com>
2020-12-29 10:39:05 +0000

    P328_OddEvenLinkedList

---
+++ src/leetcode/revision/week03/P328_OddEvenLinkedList.java
@@ -0,0 +1,65 @@
+package leetcode.revision.week03;
+
+public class P328_OddEvenLinkedList {
+
+	public static void main(String[] args) {
+
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(2);
+		ListNode l3 = new ListNode(3);
+		ListNode l4 = new ListNode(4);
+		ListNode l5 = new ListNode(5);
+		ListNode l6 = new ListNode(6);
+
+		l1.next = l2;
+		l2.next = l3;
+		l3.next = l4;
+		l4.next = l5;
+		l5.next = l6;
+
+		print(l1);
+
+		print(oddEvenList(l1));
+
+	}
+
+	static public ListNode oddEvenList(ListNode head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+
+		ListNode odd = head;
+		ListNode even = head.next;
+		ListNode evenHead = even;
+
+		while (even != null && even.next != null) {
+			odd.next = even.next;
+			odd = odd.next;
+			
+			even.next = odd.next;
+			even = even.next;
+		}
+		odd.next = evenHead;
+		return head;
+
+	}
+
+	private static void print(ListNode head) {
+		ListNode temp = head;
+		while (temp != null) {
+			System.out.print(temp.val + " ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+
+	static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+}
