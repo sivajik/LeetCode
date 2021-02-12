91a2dbcd86592c30cf56aca330d0bf7f51c29867
sivajik <sivaji_kondapalli@yahoo.com>
2021-02-12 20:49:21 +0000

    P12

---
+++ src/leetcode/thirtydays/feb/P12_StepstoZero.java
@@ -0,0 +1,23 @@
+package leetcode.thirtydays.feb;
+
+public class P12_StepstoZero {
+
+	public static void main(String[] args) {
+		System.out.println(numberOfSteps(14));
+		System.out.println(numberOfSteps(8));
+	}
+
+    public static int numberOfSteps (int num) {
+        int counter = 0;
+        
+        while (num != 0) {
+        	if (num % 2 == 1) {
+        		num--;
+        	} else {
+        		num = num / 2;
+        	}
+    		counter++;        	
+        }
+    	return counter;
+    }
+}
