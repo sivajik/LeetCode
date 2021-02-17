a38840144d465bd231bc6b9f62c5f931543d2570
sivajik <sivaji_kondapalli@yahoo.com>
2021-02-17 19:48:50 +0000

    P17_ContainerWithMostWater

---
+++ src/leetcode/thirtydays/feb/P17_ContainerWithMostWater.java
@@ -0,0 +1,34 @@
+package leetcode.thirtydays.feb;
+
+public class P17_ContainerWithMostWater {
+
+	public static void main(String[] args) {
+		System.out.println(maxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+
+	}
+
+	static public int maxArea(int[] height) {
+		// brute force
+/*		int maxArea = 0;
+		for (int i = 0; i < height.length; i++) {
+			for (int j = i + 1; j < height.length; j++) {
+				maxArea = Math.max(maxArea, (j - i) * Math.min(height[i], height[j]));
+			}
+		}
+		return maxArea;
+*/
+		// 2 pointer
+		int maxArea = 0;
+		int l = 0;
+		int r = height.length-1;
+		while (l < r) {
+			maxArea = Math.max(maxArea, (r-l) * Math.min(height[l], height[r]));
+			if (height[l] < height[r]) {
+				l++;
+			} else {
+				r--;
+			}
+		}
+		return maxArea;
+	}
+}
