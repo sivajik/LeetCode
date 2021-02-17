20af788d6c5e8614b5f57908961aad4726908065
sivajik <sivaji_kondapalli@yahoo.com>
2021-02-17 19:48:05 +0000

    Prob11_ContainerWithMostWater

--- src/leetcode/medium/Prob11_ContainerWithMostWater.java
+++ src/leetcode/medium/Prob11_ContainerWithMostWater.java
@@ -8,10 +8,25 @@ public class Prob11_ContainerWithMostWater {
 	}
 
 	static public int maxArea(int[] height) {
-		int maxArea = 0;
+		// brute force
+/*		int maxArea = 0;
 		for (int i = 0; i < height.length; i++) {
 			for (int j = i + 1; j < height.length; j++) {
 				maxArea = Math.max(maxArea, (j - i) * Math.min(height[i], height[j]));
+			}
+		}
+		return maxArea;
+*/
+		// 2 pointer
+		int maxArea = 0;
+		int l = 0;
+		int r = height.length-1;
+		while (l < r) {
+			maxArea = Math.max(maxArea, (r-l) * Math.min(height[l], height[r]));
+			if (height[l] < height[r]) {
+				l++;
+			} else {
+				r--;
 			}
 		}
 		return maxArea;
