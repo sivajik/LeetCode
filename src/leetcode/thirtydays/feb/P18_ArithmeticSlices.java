419fde40b90501839be09808264a5d81da6cd538
sivajik <sivaji_kondapalli@yahoo.com>
2021-02-18 20:37:18 +0000

    P18

---
+++ src/leetcode/thirtydays/feb/P18_ArithmeticSlices.java
@@ -0,0 +1,23 @@
+package leetcode.thirtydays.feb;
+
+public class P18_ArithmeticSlices {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	static public int numberOfArithmeticSlices(int[] A) {
+		int count = 0;
+		int dp = 0;
+		for (int i = 2; i < A.length; i++) {
+			if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+				dp++;
+				count = count + dp;
+			} else {
+				dp = 0;
+			}
+		}
+		return count;
+	}
+}
