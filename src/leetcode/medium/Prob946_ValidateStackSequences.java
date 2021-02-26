59cfd54b262ab1cb4319256323d66ef481c3ac80
sivajik <sivaji_kondapalli@yahoo.com>
2021-02-26 21:01:00 +0000

    Prob946_ValidateStackSequences

---
+++ src/leetcode/medium/Prob946_ValidateStackSequences.java
@@ -0,0 +1,24 @@
+package leetcode.medium;
+
+import java.util.Stack;
+
+public class Prob946_ValidateStackSequences {
+
+	public static void main(String[] args) {
+		System.out.println(validateStackSequences(new int[] { 1, 2, 3, 4, 5 }, new int[] { 4, 5, 3, 2, 1 }));
+	}
+
+	static public boolean validateStackSequences(int[] pushed, int[] popped) {
+		Stack<Integer> s = new Stack<>();
+		int j = 0;
+
+		for (int i = 0; i < pushed.length; i++) {
+			s.push(pushed[i]);
+			while (s.size() > 0 && s.peek() == popped[j]) {
+				s.pop();
+				j++;
+			}
+		}
+		return j == pushed.length;
+	}
+}
