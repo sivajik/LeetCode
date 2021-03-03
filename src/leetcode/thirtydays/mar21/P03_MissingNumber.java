76172484265b8b133f09effed705efc620d9b4ae
sivajik <sivaji_kondapalli@yahoo.com>
2021-03-03 19:53:04 +0000

    P03_MissingNumber

---
+++ src/leetcode/thirtydays/mar21/P03_MissingNumber.java
@@ -0,0 +1,17 @@
+package leetcode.thirtydays.mar21;
+
+public class P03_MissingNumber {
+
+	public static void main(String[] args) {
+		System.out.println(missingNumber(new int[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }));
+	}
+
+	public static int missingNumber(int[] nums) {
+		int n = nums.length;
+		int sum = 0;
+		for (int i : nums) {
+			sum += i;
+		}
+		return ((n * (n + 1)) / 2) - sum;
+	}
+}
