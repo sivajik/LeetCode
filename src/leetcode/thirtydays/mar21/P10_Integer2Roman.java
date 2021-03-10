8b1dd4c9bc0aeb045a8a06a12e853e1654ebc780
sivajik <sivaji_kondapalli@yahoo.com>
2021-03-10 21:10:02 +0000

    P10

---
+++ src/leetcode/thirtydays/mar21/P10_Integer2Roman.java
@@ -0,0 +1,36 @@
+package leetcode.thirtydays.mar21;
+
+public class P10_Integer2Roman {
+
+	public static void main(String[] args) {
+		System.out.println(intToRoman(49));
+
+	}
+
+	static public String intToRoman(int num) {
+		int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};    
+		String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+		
+		StringBuilder sbr = new StringBuilder();
+		for (int i = 0; i < values.length; i++) {
+			while (values[i] <= num) {
+				num -= values[i];
+				sbr.append(symbols[i]);
+			}
+		}
+		return sbr.toString();
+	}
+}
+
+/*
+Symbol       Value
+I             1    IV 4
+V             5    IX 9
+X             10   XL 40
+L             50   XC 90
+C             100  CD 400
+D             500  CM 900
+M             1000
+
+
+*/
