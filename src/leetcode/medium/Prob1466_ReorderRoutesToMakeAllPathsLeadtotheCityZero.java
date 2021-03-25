d159ad4c8ca5937b13344ee5b195ba22e581b873
sivajik <sivaji_kondapalli@yahoo.com>
2021-03-25 21:34:05 +0000

    1466

--- src/leetcode/medium/Prob1466_ReorderRoutesToMakeAllPathsLeadtotheCityZero.java
+++ src/leetcode/medium/Prob1466_ReorderRoutesToMakeAllPathsLeadtotheCityZero.java
@@ -11,7 +11,6 @@ public class Prob1466_ReorderRoutesToMakeAllPathsLeadtotheCityZero {
 
 	static public int minReorder(int n, int[][] connections) {
 		// [0,1],[1,3],[2,3],[4,0],[4,5]
-
 		Set<Integer> canReachZeros = new HashSet<>();
 		canReachZeros.add(0); // this is key
 
