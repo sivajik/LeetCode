449c2e2bf8afe6b3d10fcd84e999756d986c5f23
sivajik <sivaji_kondapalli@yahoo.com>
2021-04-04 10:19:01 +0100

    Prob1114_PrintinOrder

---
+++ src/leetcode/easy/Prob1114_PrintinOrder.java
@@ -0,0 +1,41 @@
+package leetcode.easy;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Prob1114_PrintinOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	static class Foo {
+
+		AtomicInteger firstOne = new AtomicInteger(0);
+		AtomicInteger secondOne = new AtomicInteger(1);
+
+		public Foo() {
+
+		}
+
+		public void first(Runnable printFirst) throws InterruptedException {
+			printFirst.run();
+			firstOne.incrementAndGet();
+		}
+
+		public void second(Runnable printSecond) throws InterruptedException {
+			while (firstOne.get() != 1) {
+
+			}
+			printSecond.run();
+			secondOne.incrementAndGet();
+		}
+
+		public void third(Runnable printThird) throws InterruptedException {
+			while (secondOne.get() != 1) {
+
+			}
+			printThird.run();
+		}
+	}
+}
