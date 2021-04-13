072a26fe2f8be09d91e9774923fac37f77d49bfd
sivajik <sivaji_kondapalli@yahoo.com>
2021-04-13 19:36:04 +0100

    Prob44_WildcardMatching

---
+++ src/leetcode/hard/Prob44_WildcardMatching.java
@@ -0,0 +1,46 @@
+package leetcode.hard;
+
+public class Prob44_WildcardMatching {
+
+	public static void main(String[] args) {
+		System.out.println(isMatch("abcd", "ab?d"));
+	}
+
+	static public boolean isMatch(String s, String p) {
+
+		int sLen = s.length();
+		int pLen = p.length();
+
+		int sIndex = 0, pIndex = 0;
+		int starIndex = -1;
+		int sTmpIdx = -1;
+		while (sIndex < sLen) {
+			if (pIndex < pLen && (p.charAt(pIndex) == '?' || p.charAt(pIndex) == s.charAt(sIndex))) {
+				pIndex++;
+				sIndex++;
+			} else if (pIndex < pLen && p.charAt(pIndex) == '*') {
+				// mean many or more
+				starIndex = pIndex;
+				sTmpIdx = sIndex;
+				pIndex++;
+			} else if (starIndex == -1) {
+				return false;
+			} else {
+				// Backtrack: check the situation
+				// when '*' matches one more character
+				pIndex = starIndex + 1;
+				sIndex = sTmpIdx + 1;
+				sTmpIdx = sIndex;
+			}
+		}
+
+		for (int i = pIndex; i < pLen; i++) {
+			if (p.charAt(i) != '*') {
+				return false;
+			}
+		}
+		return true;
+
+	}
+
+}
