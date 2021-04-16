87f66bfb6384a43196300eff9e221ebb9fda7a0a
sivajik <sivaji_kondapalli@yahoo.com>
2021-04-16 20:51:29 +0100

    Prob139_WordBreak

---
+++ src/leetcode/medium/Prob139_WordBreak.java
@@ -0,0 +1,32 @@
+package leetcode.medium;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Prob139_WordBreak {
+
+	public static void main(String[] args) {
+		System.out.println(wordBreak("leetcode", Arrays.asList(new String[] { "leet", "code" })));
+	}
+
+	static public boolean wordBreak(String s, List<String> wordDict) {
+		Set<String> set = new HashSet<>(wordDict);
+		int n = s.length();
+
+		boolean[] dp = new boolean[n + 1];
+		dp[0] = true;
+
+		for (int len = 1; len <= n; len++) {
+			for (int i = 0; i < len; i++) {
+				String temp = s.substring(i, len);
+				System.out.println("Checking : " + temp);
+				if (dp[i] && set.contains(temp)) {
+					dp[len] = true;
+				}
+			}
+		}
+		return dp[n];
+	}
+}
