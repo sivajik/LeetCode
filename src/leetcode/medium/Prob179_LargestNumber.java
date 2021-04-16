bf37d517d5c20812cc9c6ccd679d6093126cad03
sivajik <sivaji_kondapalli@yahoo.com>
2021-04-16 15:16:51 +0100

    Prob179_LargestNumber

--- src/leetcode/medium/Prob179_LargestNumber.java
+++ src/leetcode/medium/Prob179_LargestNumber.java
@@ -8,7 +8,7 @@ public class Prob179_LargestNumber {
 	public static void main(String[] args) {
 		System.out.println(largestNumber(new int[] { 10, 2 }));
 		System.out.println(largestNumber(new int[] { 3, 5, 9, 30, 34 }));
-		System.out.println(largestNumber(new int[] { 0,0 }));
+		System.out.println(largestNumber(new int[] { 10,0 }));
 	}
 
 	static public String largestNumber(int[] nums) {
