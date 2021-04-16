de2e7e590d799cd9c9a8bcd30f35fe9fdecaabd5
sivajik <sivaji_kondapalli@yahoo.com>
2021-04-16 12:15:46 +0100

    Prob8_StringtoInteger

--- src/leetcode/medium/Prob8_StringtoInteger.java
+++ src/leetcode/medium/Prob8_StringtoInteger.java
@@ -24,6 +24,9 @@ public class Prob8_StringtoInteger {
 
 		int res = 0;
 		while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+			// this is the key to handle max values & boundaries
+			// since we are multiplying res by 10
+			// if its already > max/10 OR ( it is max/10 and curr char is > max%10)
 			if (res > Integer.MAX_VALUE / 10
 					|| (res == Integer.MAX_VALUE / 10 && s.charAt(i) - '0' > Integer.MAX_VALUE % 10)) {
 				return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
