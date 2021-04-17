1db4aaf1965c7d686c01df235908e5d35fad748f
sivajik <sivaji_kondapalli@yahoo.com>
2021-04-17 10:17:15 +0100

    Prob289_GameOfLife

--- src/leetcode/medium/Prob289_GameOfLife.java
+++ src/leetcode/medium/Prob289_GameOfLife.java
@@ -14,10 +14,16 @@ public class Prob289_GameOfLife {
 
 	static public void gameOfLife(int[][] board) {
 		/*
-		 * Any live cell with fewer than two live neighbors dies as if caused by
-		 * under-population. Any live cell with two or three live neighbors lives on to
-		 * the next generation. Any live cell with more than three live neighbors dies,
-		 * as if by over-population. Any dead cell with exactly three live neighbors
+		 * Any live cell with 'fewer than two' live neighbors dies as if caused by
+		 * under-population. 
+		 * 
+		 * Any live cell with 'more than three' live neighbors dies,
+		 * as if by over-population. 
+		 * 
+		 * Any live cell with two or three live neighbors lives on to
+		 * the next generation. 
+		 * 
+		 * Any dead cell with exactly three live neighbors
 		 * becomes a live cell, as if by reproduction.
 		 */
 
