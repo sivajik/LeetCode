c774d26742972c3668b66f95c15c141860ed36cd
sivajik <sivaji_kondapalli@yahoo.com>
2021-07-09 21:07:31 +0100

    d9

---
+++ src/leetcode/thirtydays/july21/Day09_LIS.java
@@ -0,0 +1,26 @@
+package leetcode.thirtydays.july21;
+
+import java.util.Arrays;
+
+public class Day09_LIS {
+
+	public static void main(String[] args) {
+		System.out.println(lengthOfLIS(new int[] {10,9,2,5,3,7,101,18}));
+	}
+
+	static public int lengthOfLIS(int[] nums) {
+		int[] res = new int[nums.length];
+		Arrays.fill(res, 1);
+
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (nums[i] > nums[j]) {
+					res[i] = Math.max(res[i], 1 + res[j]);
+				}
+			}
+		}
+
+		Arrays.sort(res);
+		return res[res.length - 1];
+	}
+}
