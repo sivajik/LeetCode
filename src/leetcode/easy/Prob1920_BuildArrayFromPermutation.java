66ae02c51dbe5dcc3dcbe1d636dab4cf6bce0085
sivajik <sivaji_kondapalli@yahoo.com>
2022-01-18 20:56:07 +0000

    v2

--- src/leetcode/easy/Prob1920_BuildArrayFromPermutation.java
+++ src/leetcode/easy/Prob1920_BuildArrayFromPermutation.java
@@ -6,7 +6,8 @@ public class Prob1920_BuildArrayFromPermutation {
 
 	public static void main(String[] args) {
 		System.out.println(Arrays.toString(buildArray(new int[] { 5, 0, 1, 2, 3, 4 })));
-
+		System.out.println(Arrays.toString(buildArray1(new int[] { 5, 0, 1, 2, 3, 4 })));
+		// [4, 5, 0, 1, 2, 3]
 	}
 
 	static public int[] buildArray(int[] nums) {
@@ -14,6 +15,17 @@ public class Prob1920_BuildArrayFromPermutation {
 		for (int i = 0; i < nums.length; i++) {
 			res[i] = nums[nums[i]];
 		}
-		return res;
+		return nums;
+	}
+	
+	static public int[] buildArray1(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = nums[i] + nums.length * (nums[nums[i]] % nums.length);
+		}
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = nums[i] / nums.length;
+		}
+		return nums;
 	}
 }
+// 5 0 1 2 3 4
