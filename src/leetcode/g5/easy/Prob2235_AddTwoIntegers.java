1f15cef75d9652367d3b5f92821aea2dbf5acd58
sivajik <sivaji_kondapalli@yahoo.com>
2022-04-19 21:42:07 +0100

    Prob2235_AddTwoIntegers

---
+++ src/leetcode/g5/easy/Prob2235_AddTwoIntegers.java
@@ -0,0 +1,13 @@
+package leetcode.g5.easy;
+
+public class Prob2235_AddTwoIntegers {
+
+	public static void main(String[] args) {
+		System.out.println(sum(3, 6));
+
+	}
+
+	static public int sum(int num1, int num2) {
+		return num1 + num2;
+	}
+}
